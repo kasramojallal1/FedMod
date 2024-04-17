bee832dbb81fe07382021f385b789c3cccff20f8
Kasra Mojallal <kasramojallal1@gmail.com>
2024-04-17 11:17:48 -0400

    a new general function for creating nodes created

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ random.seed(45)
 
 dataset_address_1 = './datasets/boston_housing.csv'
 dataset_address_2 = './datasets/heart.csv'
-dataset_address_4 = './datasets/PhishingData.arff'
+dataset_address_4 = './datasets/phishing.arff'
 
 run_dataset_1 = False
 run_dataset_2 = False
-run_dataset_3 = False
+run_dataset_3 = True
 run_dataset_4 = False
 
 if __name__ == "__main__":
@@ -33,22 +33,23 @@ if __name__ == "__main__":
         party1, party2, server1, server2, main_server1 = nodes.create_nodes_1(X_train, y_train)
         train_test.train_parties_1(10, [party1, party2], [server1, server2], main_server1, X_test, y_test)
 
-    if run_dataset_2:
+    elif run_dataset_2:
         X_train, X_test, y_train, y_test = preprocess.setup_dataframe_2(dataset_address_2)
         party1, party2, server1, server2, main_server1 = nodes.create_nodes_2(X_train, y_train)
         run_time, cpu_usage, memory_usage = train_test.train_parties_2(30, [party1, party2], [server1, server2], main_server1, X_train, y_train, X_test, y_test)
 
-    if run_dataset_3:
+    elif run_dataset_3:
         X_train, X_test, y_train, y_test = preprocess.setup_dataframe_3()
         party1, party2, server1, server2, main_server1 = nodes.create_nodes_3(X_train, y_train, [17, 17])
-        run_time, cpu_usage, memory_usage = train_test.train_parties_3(500, party1, party2, server1, server2, main_server1, X_train, y_train, X_test,y_test)
+        run_time, cpu_usage, memory_usage = train_test.train_parties_3(30, party1, party2, server1, server2, main_server1, X_train, y_train, X_test,y_test)
 
-    if run_dataset_4:
+    elif run_dataset_4:
         X_train, X_test, y_train, y_test = preprocess.setup_dataframe_4(dataset_address_4)
         party1, party2, server1, server2, main_server1 = nodes.create_nodes_4(X_train, y_train, 2, 'multi-classification', number_of_classes=3)
-        run_time, cpu_usage, memory_usage = train_test.train_parties_4(100, [party1, party2], [server1, server2], main_server1, X_train, y_train, X_test, y_test, n_classes=3)
+        run_time, cpu_usage, memory_usage = train_test.train_parties_4(30, [party1, party2], [server1, server2], main_server1, X_train, y_train, X_test, y_test, n_classes=3)
 
-    print("------ FedMod ------")
-    print("--- FedMod: %s secs ---" % (run_time))
-    print(f"Memory Usage: {memory_usage} KB")
-    print(f"CPU Time Used: {cpu_usage} seconds")
+    if run_dataset_1 or run_dataset_2 or run_dataset_3 or run_dataset_4:
+        print("------ FedMod ------")
+        print("--- FedMod: %s secs ---" % (run_time))
+        print(f"Memory Usage: {memory_usage} KB")
+        print(f"CPU Time Used: {cpu_usage} seconds")
--- nodes.py
+++ nodes.py
@@ -142,3 +142,93 @@ def create_nodes_4(X_train, y_train, number_of_clients, problem_type, number_of_
     main_server1 = main_server.MainServer('main_server', labels=y_train)
 
     return party1, party2, server1, server2, main_server1
+
+
+import client
+import server
+import main_server
+
+import numpy as np
+
+
+def create_nodes(n_parties, n_servers, problem_type, n_classes, X_train, y_train):
+    party_list = []
+    server_list = []
+
+    parties_data = []
+    parties_weights = []
+    parties_bias = []
+
+    n_features = X_train.shape[1]
+    column_share = n_features // n_parties
+    extra_columns = n_features % n_parties
+
+    if problem_type == 'binary-classification' or problem_type == 'regression':
+        for i in range(n_parties):
+            if i == n_parties - 1:
+                data_party = X_train.iloc[:, i * column_share:]
+                weights_party = np.random.uniform(-1, 1, column_share + extra_columns)
+                bias_party = np.random.uniform(-1, 1, 1)
+            else:
+                data_party = X_train.iloc[:, i * column_share:(i + 1) * column_share]
+                weights_party = np.random.uniform(-1, 1, column_share)
+                bias_party = np.random.uniform(-1, 1, 1)
+
+            parties_data.append(data_party)
+            parties_weights.append(weights_party)
+            parties_bias.append(bias_party)
+
+    elif problem_type == 'multi-classification':
+        for i in range(n_parties):
+            if i == n_parties - 1:
+                data_party = X_train.iloc[:, i * column_share:]
+                weights_party = []
+                bias_party = []
+                for j in range(n_classes):
+                    weights_party.append(np.random.uniform(-1, 1, column_share + extra_columns))
+                    bias_party.append(np.random.uniform(-1, 1, 1))
+            else:
+                data_party = X_train.iloc[:, i * column_share:(i + 1) * column_share]
+                weights_party = []
+                bias_party = []
+                for j in range(n_classes):
+                    weights_party.append(np.random.uniform(-1, 1, column_share))
+                    bias_party.append(np.random.uniform(-1, 1, 1))
+
+            parties_data.append(data_party)
+            parties_weights.append(weights_party)
+            parties_bias.append(bias_party)
+
+    if problem_type == 'regression':
+        for i in range(n_parties):
+            if i == 0:
+                party_list.append(client.Client(name=f'party{i + 1}',
+                                                weights=parties_weights[i],
+                                                bias=parties_bias[i],
+                                                data=parties_data[i],
+                                                lead=1,
+                                                labels=y_train))
+            else:
+                party_list.append(client.Client(name=f'party{i + 1}',
+                                                weights=parties_weights[i],
+                                                bias=parties_bias[i],
+                                                data=parties_data[i],
+                                                lead=0))
+
+    else:
+        for i in range(n_parties):
+            party_list.append(client.Client(name=f'party{i + 1}',
+                                            weights=parties_weights[i],
+                                            bias=parties_bias[i],
+                                            data=parties_data[i],
+                                            lead=0))
+
+    for i in range(n_servers):
+        server_list.append(server.Server(f'server{i + 1}'))
+
+    if problem_type != 'regression':
+        output_main_server = main_server.MainServer('main_server', labels=y_train)
+    else:
+        output_main_server = main_server.MainServer('main_server')
+
+    return party_list, server_list, output_main_server
